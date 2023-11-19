d04ac4638d67e42c5a7305b17da946cfcb75c003
Itm-info <itm@infotech.net.ua>
2023-11-19 19:23:28 +0200

    Initial Commit

--- src/main/java/itm/GoitDevHw12Application.java
+++ src/main/java/itm/GoitDevHw12Application.java
@@ -1,12 +1,12 @@
 package itm;
 
+import itm.note.Note;
+import itm.note.NoteService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GoitDevHw12Application {
-	public static void main(String[] args) {
-		SpringApplication.run(GoitDevHw12Application.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(GoitDevHw12Application.class, args);	}
 
 }
